3b4d033981cb74f5e334c60783a4eeb440ee25a0
Pavel Filatov <pavel.filatov@wmglobal.com>
2018-04-11 16:47:33 +0300

    now functions work

--- R/infix.R
+++ R/infix.R
@@ -2,52 +2,64 @@
 #' @importFrom purrr map_lgl
 
 infix_input_checker <- function(...) {
-  .dots <- list(...)
-  if (!all(purrr::map_lgl(.dots, is.numeric))) {
-    stop(message("Only numeric inputs allowed!"))
+  if (!all(purrr::map_lgl(list(...), is.numeric))) {
+    stop("Only numeric inputs allowed!")
   }
+}
+
+get_lhs_env <- function(.lhs) {
+  .lhs_quo <- rlang::enquo(.lhs)
+  rlang::get_env(.lhs_quo)
 }
 
 #' @export
 "%+=%" <- function(.lhs, .rhs) {
+  .lhs_quo <- rlang::enquo(.lhs)
+  .lhs_env <- rlang::get_env(.lhs_quo)
+
   infix_input_checker(.lhs, .rhs)
 
-  .lhs_quo <- rlang::enquo(.lhs)
   .lhs_name <- rlang::quo_name(.lhs_quo)
 
-  assign(.lhs_name, .lhs + .rhs, envir = rlang::get_env(.lhs_quo))
-}
-
-#' @export
-"%-=%" <- function(.lhs, .rhs) {
-  infix_input_checker(.lhs, .rhs)
-
-  .lhs_quo <- rlang::enquo(.lhs)
-  .lhs_name <- rlang::quo_name(.lhs_quo)
-
-  assign(.lhs_name, .lhs - .rhs, envir = rlang::get_env(.lhs_quo))
+  assign(.lhs_name, .lhs + .rhs, envir = .lhs_env)
 }
 
 #' @export
 "%*=%" <- function(.lhs, .rhs) {
+  .lhs_quo <- rlang::enquo(.lhs)
+  .lhs_env <- rlang::get_env(.lhs_quo)
+
   infix_input_checker(.lhs, .rhs)
 
-  .lhs_quo <- rlang::enquo(.lhs)
   .lhs_name <- rlang::quo_name(.lhs_quo)
 
-  assign(.lhs_name, .lhs * .rhs, envir = rlang::get_env(.lhs_quo))
+  assign(.lhs_name, .lhs * .rhs, envir = .lhs_env)
+}
+
+#' @export
+"%-=%" <- function(.lhs, .rhs) {
+  .lhs_quo <- rlang::enquo(.lhs)
+  .lhs_env <- rlang::get_env(.lhs_quo)
+
+  infix_input_checker(.lhs, .rhs)
+
+  .lhs_name <- rlang::quo_name(.lhs_quo)
+
+  assign(.lhs_name, .lhs - .rhs, envir = .lhs_env)
 }
 
 #' @export
 "%/=%" <- function(.lhs, .rhs) {
-  infix_input_checker(.lhs, .rhs)
-
   if (.rhs == 0) {
-    message("You divide by zero!")
+    message("Zero dividing!")
   }
 
   .lhs_quo <- rlang::enquo(.lhs)
+  .lhs_env <- rlang::get_env(.lhs_quo)
+
+  infix_input_checker(.lhs, .rhs)
+
   .lhs_name <- rlang::quo_name(.lhs_quo)
 
-  assign(.lhs_name, .lhs / .rhs, envir = rlang::get_env(.lhs_quo))
+  assign(.lhs_name, .lhs / .rhs, envir = .lhs_env)
 }
